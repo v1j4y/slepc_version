2275d499e3da5858773a8787fbd6fc53de676db5
v1j4y <vijay.gopal.c@gmail.com>
2015-04-15 17:10:11 +0200

    mirror of commit in eos server: working ofr n<24 (added provide_clust.irp.f)

---
+++ provide_clust.irp.f
@@ -0,0 +1,21 @@
+BEGIN_PROVIDER[integer,l1,   (maxlien)]
+&BEGIN_PROVIDER[integer,l2,  (maxlien)]
+&BEGIN_PROVIDER[integer,ktyp,(maxlien)]
+      implicit none
+      integer::i
+!       open(unit=11,file="l1.dat",form="formatted")
+!       open(unit=12,file="l2.dat",form="formatted")
+!       open(unit=13,file="ktyp.dat",form="formatted")
+!       rewind(11)
+!       rewind(12)
+!       rewind(13)
+!       do i=1,42
+!       read(11,*)l1(i)
+!       print *,l1(i)
+!       read(12,*)l2(i)
+!       read(13,*)ktyp(i)
+!       enddo
+!       close(11)
+!       close(12)
+!       close(13)
+END_PROVIDER
